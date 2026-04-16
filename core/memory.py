edfb2be15594a78cd76e89b56a6940fa7f4c7120
Bhavni Sharma <bhavni5831sjps@gmail.com>
2026-04-16 09:55:32 +0530

    Delete core directory

--- core/memory.py
+++
@@ -1,7 +0,0 @@
-class Memory:
-    def __init__(self, size):
-        self.size = size
-        self.frames = [None] * size
-
-    def load_page(self, index, page):
-        self.frames[index] = page
--- core/process.py
+++
@@ -1,4 +0,0 @@
-class Process:
-    def __init__(self, pid, pages):
-        self.pid = pid
-        self.pages = pages
